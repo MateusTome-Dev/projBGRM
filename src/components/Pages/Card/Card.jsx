2c5b12dd60d4e1c4017444b4d4d96b23f9d446a0
Mateus Tomé <154273310+MateusTome-Dev@users.noreply.github.com>
2024-06-09 23:56:05 -0300

    third commit

--- src/components/Pages/Card/Card.jsx
+++ src/components/Pages/Card/Card.jsx
@@ -4,59 +4,59 @@ import Alagamento from '/public/img/alagamento.jpg';
 import Animais from '/public/img/animais.jpg';
 import Desabamento from  '/public/img/desmoronamento.jpg';
 import Incendio from '/public/img/incêndio.jpg';
-import Arvore from '/public/img/queda-arvore.jpg';
+import PrimeiroSocorros from '/public/img/primeiros-socorros.png';
 function Card() {
     return (
         <>
-            <section className="section">
+            <section className="sectionCards">
                 <div className="container">
                     <div className="card__container">
                         <article className="card__article">
-                            <img src={Arvore} alt="Queda de Árvore" className="card__img" />
+                            <img src={PrimeiroSocorros} alt="Queda de Árvore" className="card__img" />
                             <div className="card__data">
-                                <span className="card__description">***</span>
-                                <h2 className="card__title">Queda de Árvore</h2>
-                                <a href="#" className="card__button">Read More</a>
+                                
+                                <h2 className="card__title">Primeiros socorros</h2>
+                                <a href="http://www.ccb.policiamilitar.sp.gov.br/portalcb/_educacao-publica/midias-edpub/folders/pdf/PRIMEIROS-SOCORROS.pdf" target="_blank" className="card__button">Read More</a>
                             </div>
                         </article>
                         <article className="card__article">
                             <img src={Alagamento} alt="Alagamento" className="card__img" />
                             <div className="card__data">
-                                <span className="card__description">***</span>
+                                
                                 <h2 className="card__title">Alagamento</h2>
-                                <a href="#" className="card__button">Read More</a>
+                                <a href="http://www.ccb.policiamilitar.sp.gov.br/portalcb/_educacao-publica/midias-edpub/folders/pdf/ENCHENTES.pdf" target="_blank" className="card__button">Read More</a>
                             </div>
                         </article>
                         <article className="card__article">
                             <img src={Desabamento} alt="Desabamento" className="card__img" />
                             <div className="card__data">
-                                <span className="card__description">***</span>
+                                
                                 <h2 className="card__title">Desabamento</h2>
-                                <a href="#" className="card__button">Read More</a>
+                                <a href="http://www.ccb.policiamilitar.sp.gov.br/portalcb/_educacao-publica/midias-edpub/folders/pdf/EDIFICACAO-MAIS-SEGURA.pdf" target="_blank" className="card__button">Read More</a>
                             </div>
                         </article>
                         <article className="card__article">
                             <img src={Incendio} alt="Incêndio Florestal" className="card__img" />
                             <div className="card__data">
-                                <span className="card__description">***</span>
+                               
                                 <h2 className="card__title">Incêndio Florestal</h2>
-                                <a href="#" className="card__button">Read More</a>
+                                <a href="http://www.ccb.policiamilitar.sp.gov.br/portalcb/_educacao-publica/midias-edpub/folders/pdf/INCENDIO-EM-VEGETACAO.pdf" target="_blank" className="card__button">Read More</a>
                             </div>
                         </article>
                         <article className="card__article">
                             <img src={Animais} alt="Acidente com animais" className="card__img" />
                             <div className="card__data">
-                                <span className="card__description">***</span>
+                                
                                 <h2 className="card__title">Acidente com animais</h2>
-                                <a href="#" className="card__button">Read More</a>
+                                <a href="http://www.ccb.policiamilitar.sp.gov.br/portalcb/_educacao-publica/midias-edpub/folders/pdf/ACIDENTES-COM-ANIMAIS-PECONHENTOS.pdf" target="_blank" className="card__button">Read More</a>
                             </div>
                         </article>
                         <article className="card__article">
                             <img src={Acidente} alt="Acidente" className="card__img" />
                             <div className="card__data">
-                                <span className="card__description">***</span>
+                                
                                 <h2 className="card__title">Acidente</h2>
-                                <a href="#" className="card__button">Read More</a>
+                                <a href="http://www.ccb.policiamilitar.sp.gov.br/portalcb/_educacao-publica/midias-edpub/folders/pdf/ACIDENTE-DE-TRANSITO.pdf" target="_blank" className="card__button">Read More</a>
                             </div>
                         </article>
                     </div>
